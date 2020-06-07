8c9a1e3a49460897ffb5255ca894674e951f3235
Oscar Nunez <taw.oscarn@gmail.com>
2020-06-07 00:27:22 -0500

    inicio

---
+++ precache-manifest.f8bffa6186d40be37a62ae344e22e89c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90df0bdea25806c6a8c938022997a9fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "129353220742fa514d51",
+    "url": "/static/css/main.3732ad40.chunk.css"
+  },
+  {
+    "revision": "b49132993068ea3ad6f4",
+    "url": "/static/js/2.0eacabda.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.0eacabda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "129353220742fa514d51",
+    "url": "/static/js/main.a778ae48.chunk.js"
+  },
+  {
+    "revision": "de1768ec11fce88c0538",
+    "url": "/static/js/runtime-main.98641e41.js"
+  },
+  {
+    "revision": "09fdb3968cecb063ad52922127186e85",
+    "url": "/static/media/COMPLIACNE_035.09fdb396.png"
+  }
+]);
